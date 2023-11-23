c22ed147297eb4198b35f72173f3afc5bccacaf6
Hexdigest <pierre.maurice.m256@googlemail.com>
2023-11-24 00:23:12 +0100

    finished climbing_stairs in rust

---
+++ easy/climbing_stairs/src/main.rs
@@ -0,0 +1,15 @@
+pub fn climb_stairs(n: i32) -> i32{
+    let n = n as usize;
+    let mut dp = vec![0; n + 1];
+    dp[0] = 1;
+    dp[1] = 1;
+    for i in 2..=n {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    dp[n]
+}
+fn main() {
+    println!("{}", climb_stairs(3));
+    println!("{}", climb_stairs(2));
+    println!("{}", climb_stairs(4));
+}
